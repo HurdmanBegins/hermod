82d4600a85345f2b0ced98485a8bd8390deb7c7c
Hooligan-0 <gwen-github@agilack.fr>
2016-09-01 13:33:38 +0200

    = UPD: Allow to run hermod into background (as daemon)

--- src/main.cpp
+++ src/main.cpp
@@ -17,24 +17,35 @@
 #include <iostream>
 #include <string>
 #include <signal.h>
+#include <unistd.h>
 #include "App.hpp"
 #include "Config.hpp"
 
 static void config(int argc, char **argv);
+static void daemonize(void);
 static void signal_handler(int sig);
 
+/**
+ * @brief Entry point of hermod
+ *
+ * @param argc Number of arguments on command line
+ * @param argv Pointer to arguments array
+ */
 int main(int argc, char **argv)
 {
 	struct sigaction sa;
 
 	config(argc, argv);
 
+	daemonize();
+
 	// Initialise signal handler
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags   = 0;
 	sa.sa_handler = signal_handler;
 	// Install signal handler
-	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGINT,  &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
 
 	App::getInstance()->init()->exec();
 	App::destroy();
@@ -42,20 +53,33 @@ int main(int argc, char **argv)
 	return(0);
 }
 
+/**
+ * @brief Load configuration, and parse command line arguments
+ *
+ * @param argc Number of arguments from command line
+ * @param argv Array of arguments
+ */
 static void config(int argc, char **argv)
 {
 	std::string cfgFilename(DEF_CFG_FILE);
+	bool cfgDaemon = true;
 
 	try {
 		for (int i = 1; i < argc; i++)
 		{
 			bool hasMore = ( (i + 1) < argc);
+			// -c : specify a config file
 			if (std::string("-c").compare(argv[i]) == 0)
 			{
 				if ( ! hasMore)
 					throw runtime_error("Missing argument for -c");
 				i++;
 				cfgFilename = argv[i];
+			}
+			// -f : strat foreground (do not daemonize)
+			if (std::string("-f").compare(argv[i]) == 0)
+			{
+				cfgDaemon = false;
 			}
 		}
 	} catch (std::exception &e) {
@@ -67,6 +91,7 @@ static void config(int argc, char **argv)
 	// 
 	try {
 		cfg->load(cfgFilename);
+		std::cout << "Load configuration file " << cfgFilename << std::endl;
 	} catch (std::exception &e) {
 		std::cerr << "Failed to load config file " << cfgFilename << std::endl;
 	}
@@ -81,10 +106,69 @@ static void config(int argc, char **argv)
 		cfg->set("global", "session_mode", "cookie");
 	if ( cfg->get("plugins", "directory").empty() )
 		cfg->set("plugins", "directory", DEF_DIR_PLUGINS);
+	// If the "-f" flag is set, this value override config
+	if ( cfgDaemon == false )
+		cfg->set("global", "daemon", "no");
+	// Else, if no no value has been readed for "daemon", set true
+	else if ( cfg->get("global", "daemon").empty() )
+		cfg->set("global", "daemon", "yes");
 }
 
+/**
+ * @brief Detach app from his parent, go background (as daemon)
+ *
+ */
+static void daemonize(void)
+{
+	try {
+		// Get access to config
+		Config *cfg = Config::getInstance();
+		if (cfg == 0)
+			throw 1;
+
+		// Check if this instance need to be run in background
+		ConfigKey *key = cfg->getKey("global", "daemon");
+		if (key == 0)
+			throw 2;
+		if (key->getBoolean(true) == false)
+			throw 0;
+	} catch (...) {
+		std::cout << "Start in foreground mode." << std::endl;
+		return;
+	}
+	
+	// ok, here we know that we must daemonize
+	
+	pid_t pid = fork();
+	if (pid < 0)
+	{
+		std::cerr << "Error during fork() ... FATAL" << std::endl;
+		exit(-1);
+	}
+	// End of the parent process
+	if (pid > 0)
+	{
+		std::cerr << "Daemon started (pid=" << pid << ") :-)" << std::endl;
+		exit(0);
+	}
+	
+	// The child (now daemon) continue
+	close(0);
+	close(1);
+	close(2);
+	return;
+}
+
+/**
+ * @brief Function called when a system signal is catch
+ *
+ * @param sig ID of the received signal
+ */
 static void signal_handler(int sig)
 {
 	if (sig == SIGINT)
 		App::sigInt();
+	if (sig == SIGTERM)
+		App::sigInt();
 }
+/* EOF */
